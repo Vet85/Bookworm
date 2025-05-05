a3663c4c3ce59e5815eb7e2acb9fef53521d363d
Vitaliy Novichenko <novichenkovitaliy@gmail.com>
2025-05-05 09:54:15 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Vitaliy Novichenko on 05.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
