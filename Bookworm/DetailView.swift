f25de1a206d1f40bcf774b909483a153a92bf242
Vitaliy Novichenko <novichenkovitaliy@gmail.com>
2025-05-11 20:03:54 +0300

    done

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Vitaliy Novichenko on 11.05.2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ Bookworm/EmogiRatingView.swift
@@ -0,0 +1,18 @@
+//
+//  EmogiRatingView.swift
+//  Bookworm
+//
+//  Created by Vitaliy Novichenko on 09.05.2025.
+//
+
+import SwiftUI
+
+struct EmogiRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmogiRatingView()
+}
